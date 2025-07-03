31ac4703845eae4fd32c828eb438f50960abcc58
Asad Ali <[asad.ar451@gmail.com]>
2025-07-03 09:22:56 +0500

    Implement Express server with basic routing and logging functionality

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
-const myServer = http.createServer((req, res) => {
+
+const myHandler = (req, res) => {
   if (req.url === "/favicon.ico") res.end();
   const myUrl = url.parse(req.url);
   const now = new Date().toLocaleString();
-  const log = `${now}: ${req.url} New Req Received\n`;
+  const log = `${now}: ${req.method} ${req.url} New Req Received\n`;
   fs.appendFile("./log.text", log, (err, data) => {
     switch (myUrl.pathname) {
       case "/":
@@ -15,11 +16,19 @@ const myServer = http.createServer((req, res) => {
         res.end("My self Asad Ali");
         break;
 
+      case "/signup":
+        if (req.method === "GET") {
+          res.end("Sign Up Form");
+        }
+        break;
       default:
         res.end("404 Not Found!!");
         break;
     }
   });
-});
+};
+
+
+const myServer = http.createServer(myHandler);
 
 myServer.listen(3000, () => console.log("Server Started!"));
--- server/url.js
+++ server/url.js
@@ -9,3 +9,11 @@
 //  /about/projects  <---- this one the nested path 
 //  /quaryprams  <---- this one the  extra info it's always comes after (?)  quesmark in this we can pass the key value pairs 
 
+
+// now add  next topic is http methods
+// 1 : GET --> if you want to get some data from  the server.
+// 2 : POST --> When you want t0 to post the date  to server.
+// 3: PUT --> If you want to put some data like upload images and documents.
+// 4 :PATCH --> If you want to update the existing date like change the username.
+// 5:DELETE --> If you want to delete some date from the server 
+
