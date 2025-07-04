043a534d4ba7427e016782d8534aeb9aafc75ee7
Asad Ali <[asad.ar451@gmail.com]>
2025-07-04 22:16:31 +0500

    Add package.json for project setup and update url.js with HTTP methods and dynamic path parameters comments

--- server/url.js
+++ server/url.js
@@ -6,14 +6,17 @@
 // https:// (hypertext transfer protocol secure)  this is protocol(set of rules that tells the browser how to communicate)۔
 // www.google.com/  its called domain : userfriendly name of  the ip address of the server
 // / <--- path like its  home path or home route
-//  /about/projects  <---- this one the nested path 
-//  /quaryprams  <---- this one the  extra info it's always comes after (?)  quesmark in this we can pass the key value pairs 
-
+//  /about/projects  <---- this one the nested path
+//  /quaryprams  <---- this one the  extra info it's always comes after (?)  quesmark in this we can pass the key value pairs
 
 // now add  next topic is http methods
 // 1 : GET --> if you want to get some data from  the server.
 // 2 : POST --> When you want t0 to post the date  to server.
 // 3: PUT --> If you want to put some data like upload images and documents.
 // 4 :PATCH --> If you want to update the existing date like change the username.
-// 5:DELETE --> If you want to delete some date from the server 
+// 5:DELETE --> If you want to delete some date from the server
 
+// dynamic path parameters
+
+// like api/users/:id
+// :id  is variable dynamic variable
