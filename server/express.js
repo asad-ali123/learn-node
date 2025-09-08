68d56243c4d8e109fa3e39deee45f867e26a22ff
Asad Ali <[asad.ar451@gmail.com]>
2025-09-08 17:32:38 +0500

    update

--- server/express.js
+++ server/express.js
@@ -1,14 +1,24 @@
-const express = require('express');
+const express = require("express");
 
 // using express  create  handler
-const app = express()
+const app = express();
+
 app.get("/", (req, res) => {
   return res.send("Hello from Home page ");
 });
 
-app.get('/about' , (req , res)=>{
-  return res.send('Hello from About' + ' Hey ' +  req.query.name + ' your age is ' + req.query.age)
-})
+app.get("/about", (req, res) => {
+  return res.send(
+    "Hello from About" +
+      " Hey " +
+      req.query.name +
+      " your age is " +
+      req.query.age
+  );
+});
 
+app.use((req, res) => {
+  res.status(404).send("Page not found 😢");
+});
 
-app.listen(3000, () => console.log("Server Started!"));
+app.listen(3000, () => console.log("Server Started at 3000"));
