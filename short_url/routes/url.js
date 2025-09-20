82e7d1bfb9434c03f5583555893439a882636402
Asad Ali <[asad.ar451@gmail.com]>
2025-09-20 23:37:47 +0500

    update

--- short_url/routes/url.js
+++ short_url/routes/url.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { handelGenerateShortUrl, handleGetAllUrl, handleGetUrlAndUpdate } from "../controllers/url.js";
+import {
+  handelGenerateShortUrl,
+  handleGetAllUrl,
+  handleGetAnalytic,
+  handleGetUrlAndUpdate,
+} from "../controllers/url.js";
 const router = express.Router();
 
 router.post("/", handelGenerateShortUrl);
 router.get("/all", handleGetAllUrl);
 router.get("/:shortId", handleGetUrlAndUpdate);
+router.get("/analytics/:id", handleGetAnalytic);
 
 export default router;
