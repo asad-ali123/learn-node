ac73d5457f90a675d61168441aba28c0ee54ee9f
Asad Ali <[asad.ar451@gmail.com]>
2025-09-25 22:25:54 +0500

    update

--- short_url/models/user.js
+++ short_url/models/user.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema(
     },
     // gender: {
     //   type: String,
-    //   enum: ["male", "female", "other"],
+    //   enum: ["male", "female", "other"], its a specal type
     // },
   },
   { timestamps: true }
