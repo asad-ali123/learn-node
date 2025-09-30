7cab338aa484d7edf13ed2c704864cc97deae170
Asad Ali <[asad.ar451@gmail.com]>
2025-09-30 14:35:59 +0500

    refactor: implement JWT authentication and update user session handling

--- short_url/middlewares/auth.js
+++ short_url/middlewares/auth.js
@@ -2,13 +2,12 @@ import { getUser } from "../service/auth.js";
 
 export async function restrictToLoggedInUserOnly(req, res, next) {
   const userUId = req.cookies.uid;
-//   if (!userUId) return res.redirect("/login"); this use for auth
+  if (!userUId) return res.redirect("/login"); //this use for auth
 
   const user = getUser(userUId);
   console.log(user);
 
-  if (!user) return res.redirect("/");
+  if (!user) return res.redirect("/login");
   req.user = user;
   next();
-  
 }
--- short_url/service/auth.js
+++ short_url/service/auth.js
@@ -1,12 +1,33 @@
-const sessionIdToUserMap = new Map();
+// use for JWT authentication
 
-export function setUser(id, user) {
-  console.log("SET USER:", id, user);
-  return sessionIdToUserMap.set(id, user);
-}
+import JWT from "jsonwebtoken";
+const secret = "asad@1234";
 
-export function getUser(id) {
-  console.log("GET USER:", id, sessionIdToUserMap);
-  return sessionIdToUserMap.get(id);
-}
+const setUser = (user) => {
+  return JWT.sign(
+    {
+      _id: user?._id,
+      email: user?.email,
+    },
+    secret
+  );
+};
 
+const getUser = (token) => {
+  return JWT.verify(token, secret);
+};
+
+export { setUser, getUser };
+
+// use for statefull authentication
+// const sessionIdToUserMap = new Map();
+
+// export function setUser(id, user) {
+//   console.log("SET USER:", id, user);
+//   return sessionIdToUserMap.set(id, user);
+// }
+
+// export function getUser(id) {
+//   console.log("GET USER:", id, sessionIdToUserMap);
+//   return sessionIdToUserMap.get(id);
+// }
