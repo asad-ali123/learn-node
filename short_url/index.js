fd81810bc33802191ce319cf89be2631a8295e36
Asad Ali <[asad.ar451@gmail.com]>
2025-10-04 13:28:58 +0500

    update

--- short_url/index.js
+++ short_url/index.js
@@ -6,7 +6,7 @@ import connectToMongoDB from "./connectDB.js";
 import path from "path";
 import URL from "./models/url.js";
 import cookieParser from "cookie-parser";
-import { restrictToLoggedInUserOnly } from "./middlewares/auth.js";
+import { checkForAuthentication, restrictTo } from "./middlewares/auth.js";
 const app = express();
 const PORT = 3000;
 
@@ -17,6 +17,7 @@ connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() =>
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(checkForAuthentication);
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
@@ -26,7 +27,7 @@ app.get("/test", async (req, res) => {
   return res.render("home", { urls: allUrls });
 });
 
-app.use("/url", restrictToLoggedInUserOnly, urlRoute); //for short-Url
+app.use("/url", restrictTo(["NORMAL", "ADMIN"]), urlRoute); //for short-Url
 app.use("/", staticRoute); //for static route
 app.use("/user", userRoute); // for user
 app.listen(PORT, () => console.log(`Server is Started at Port ${PORT}`));
--- short_url/middlewares/auth.js
+++ short_url/middlewares/auth.js
@@ -1,20 +1,26 @@
 import { getUser } from "../service/auth.js";
 function checkForAuthentication(req, res, next) {
-  const authHeader = req.headers["authorization"];
+  // const authHeader = req.headers["authorization"]; // if use header auth
+  const authCookie = req.cookies?.token; //  if use cookies
   req.user = null;
-  if (!authHeader || authHeader.startsWith("Bearer")) return next;
+  // if (!authHeader || !authHeader.startsWith("Bearer")) return next(); // if use header auth
+  if (!authCookie) return next(); // if use cookies
 
-  const token = authHeader.authHeader.split("Bearer ")[1];
+  const token = authCookie;
   const user = getUser(token);
   req.user = user;
   return next();
 }
 
-function restrictTo(roles) {
+function restrictTo(roles = []) {
   return function (req, res, next) {
     if (!req.user) return res.redirect("/login");
+    if (!roles.includes(req.user.role)) return res.end("UnAuthorized");
+
+    return next();
   };
 }
+export { checkForAuthentication, restrictTo };
 
 // export async function restrictToLoggedInUserOnly(req, res, next) {
 //   // const userUId = req.cookies.uid; validate from cookies
